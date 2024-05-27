5db162f6deeb5acdb327e64851ed0b7d67dc7a58
Samuel Damasceno Sales <132498977+busaikuna@users.noreply.github.com>
2024-05-27 19:16:39 -0300

    Web Scrapper com Puppeteer
    Comandos adicionados no chat:
    
    ./img (quantidade de imagens) (pesquisa da imagem): Esse comando abre o Puppeteer e faz ele pesquisar a imagens no google, trazendo o src delas.
    
    ./gpt (texto): Esse comando abre o Puppeter e manda o texto para o chat GPT, trazendo o textContent para o chat.

--- client/public/client.js
+++ client/public/client.js
@@ -2,6 +2,10 @@ var socketURL = window.location.protocol + "//" + window.location.hostname + (wi
 var socket = io(socketURL);
 console.log(socketURL)
 
+document.addEventListener("load", () => {
+    socket.on("online")
+})
+
 let button = document.querySelector(".button");
 let view = document.querySelector(".view");
 let viewMessages = document.querySelector(".view-messages")
@@ -19,22 +23,82 @@ function send() {
     let inputElement = document.querySelector(".input");
     let input = inputElement.value;
 
-    if(input){
+    if (input) {
         let msg = document.createElement("li");
         msg.classList.add("your-message")
         msg.textContent = input;
         viewMessages.appendChild(msg);
-    
-        socket.emit("msg", { msg: input, username: localStorage.getItem('username')});
+
+        socket.emit("msg", { msg: input, username: localStorage.getItem('username') });
         console.log(input);
         inputElement.value = "";
-    
+
         view.scrollTop = view.scrollHeight;
     }
 }
 
+socket.on("imgSearch", (data) => {
+    if (data.response) {
+        const messages = document.querySelector('.view-messages')
+
+        messages.innerHTML += `
+            <div class="another-div-message">
+                <li class="username">bot IMG</li>
+                <li class="another-message">${data.response}</li>
+            </div>
+        `
+        view.scrollTop = view.scrollHeight;
+    } else {
+        const messages = document.querySelector('.view-messages')
+
+        messages.innerHTML += `
+    <div class="another-div-message">
+    <li class="username">Bot IMG</li>
+    <img class="sendImg" src="${data.imageUrl}"></img>
+</div>
+    `
+        view.scrollTop = view.scrollHeight;
+        console.log(data)
+    }
+})
+
+socket.on("gpt", (data) => {
+    if(data.processingGpt){
+        const messages = document.querySelector('.view-messages')
+
+        messages.innerHTML += `
+            <div class="another-div-message">
+                <li class="username">CHAT GPT</li>
+                <li class="another-message">${data.processingGpt}</li>
+            </div>
+        `
+        view.scrollTop = view.scrollHeight;   
+    }else if (data.response){
+        const messages = document.querySelector('.view-messages')
+
+        messages.innerHTML += `
+            <div class="another-div-message">
+                <li class="username">CHAT GPT</li>
+                <li class="another-message">${data.response}</li>
+            </div>
+        `
+        view.scrollTop = view.scrollHeight;     
+    }else{
+        const messages = document.querySelector('.view-messages')
+
+    messages.innerHTML += `
+        <div class="another-div-message">
+            <li class="username">CHAT GPT</li>
+            <li class="another-message">${data.msg}</li>
+        </div>
+    `
+    view.scrollTop = view.scrollHeight;
+    }
+})
+
+
 socket.on("newMessage", (data) => {
-    if(socket.id != data.id){
+    if (socket.id != data.id) {
         const messages = document.querySelector('.view-messages')
 
         messages.innerHTML += `
--- server/src/server.js
+++ server/src/server.js
@@ -2,25 +2,137 @@ const http = require("http");
 const express = require("express");
 const app = express();
 const path = require("path");
+const puppeteer = require("puppeteer");
+const socketIO = require("socket.io");
 
 const httpServer = http.createServer(app);
-const io = require("socket.io")(httpServer);
+const io = socketIO(httpServer);
 
-let users = []
+let gptProcess = false;
+let imgProcess = false;
+
+let users = [];
 
 app.use(express.static(path.join(__dirname, "../../client/public/")));
 
-io.on("connection", socket => {
+io.on("connection", (socket) => {
     console.log(`Usuário conectado com ID: ${socket.id}`);
     console.log(`Endereço IP do usuário: ${socket.handshake.address}`);
-    socket.on("msg", (mensagem)=>{
-        console.log(mensagem)
-        io.emit("newMessage", {msg: mensagem.msg, id: socket.id, nick: mensagem.username})
-    })
-    socket.on("login", (data)=>{
-        users.push({[socket.id]: data.username})
-    })
+
+    socket.on("disconnect", () => {
+        users = users.filter((user) => user.id !== socket.id);
+        updateUsers();
+    });
+
+    socket.on("online", (data) => {
+        users.push({ id: socket.id, username: data.username });
+        updateUsers();
+    });
+
+    socket.on("msg", async (mensagem) => {
+        if (mensagem.msg.includes("./img")) {
+            if (!imgProcess) {
+                imgProcess = true;
+                console.log(mensagem);
+
+                let replaceSearch = mensagem.msg.replace("./img ", "").trim();
+                const pesquisaImg = replaceSearch.substring(1).replace(" ", "+");
+                const quantImg = parseInt(replaceSearch[0]);
+
+                (async () => {
+                    try {
+                        const browser = await puppeteer.launch({ headless: false });
+                        const page = await browser.newPage();
+
+                        await page.goto(
+                            `https://www.google.com/search?sca_esv=cb7836cf60c7f552&q=${pesquisaImg}&tbm=isch&source=lnms&prmd=isvnbmtz&sa=X&ved=2ahUKEwjc9dCntauGAxW2qZUCHdNYDRIQ0pQJegQIERAB&biw=800&bih=600`,
+                            { waitUntil: "networkidle2" }
+                        );
+
+                        await page.waitForSelector(
+                            "#rso > div > div > div.wH6SXe.u32vCb > div > div img",
+                            { visible: true }
+                        );
+
+                        for (let i = 0; i < quantImg; i++) {
+                            const imageUrl = await page.evaluate((index) => {
+                                const imgs = document.querySelectorAll(
+                                    "#rso > div > div > div.wH6SXe.u32vCb > div > div img"
+                                );
+                                const img = imgs[index];
+                                return img ? img.src : null;
+                            }, i * 2);
+
+                            if (imageUrl) {
+                                io.emit("imgSearch", { imageUrl });
+                                console.log("Imagem enviada:", imageUrl);
+                            }
+                        }
+
+                        await browser.close();
+                    } catch (error) {
+                        console.error("Erro durante a execução do script:", error);
+                    } finally {
+                        imgProcess = false;
+                    }
+                })();
+            } else {
+                let response = "Calma ai cara, eu sou só um!! 😡";
+                io.emit("imgSearch", { response });
+            }
+        }
+
+        if (mensagem.msg.includes("./gpt")) {
+            if (!gptProcess) {
+                gptProcess = true;
+                let processingGpt = "Estou pensando no que responder aqui, pera ai.";
+                io.emit("gpt", { processingGpt });
+                console.log(mensagem);
+
+                (async () => {
+                    try {
+                        const browser = await puppeteer.launch({ headless: false});
+                        const page = await browser.newPage();
+
+                        await page.goto('https://chatgpt.com/', { waitUntil: 'networkidle2' });
+                        await page.waitForSelector("#prompt-textarea", { visible: true });
+
+                        await page.click("#prompt-textarea");
+                        await page.type("#prompt-textarea", `${mensagem.msg.replace("./gpt", "")}`);
+                        await page.keyboard.press("Enter");
+
+                        await page.waitForSelector("div > main > div > div > div > div > div > div > div > div > div > div > div > div > div > span > button > svg", {visible: true});
+
+                        await new Promise((resolve) => setTimeout(resolve, 2000));
+                        const resposta = await page.evaluate(() => {
+                            const element = document.querySelector("div > div > main > div > div > div > div > div > div > div:nth-child(3) > div > div");
+                            return element.textContent.replace("ChatGPTChatGPT", "");
+                        });
+
+                        console.log(resposta);
+                        io.emit("gpt", { msg: resposta.replace("#fim", "") });
+                        await browser.close()
+                    } catch (error) {
+                        console.error("Erro durante a execução do script:", error);
+                    } finally {
+                        gptProcess = false;
+                    }
+                })();
+            } else {
+                let response = "Calma ai, só consigo pensar em uma coisa por vez!! 😔";
+                io.emit("gpt", { response });
+            }
+        }
+
+        io.emit("newMessage", { msg: mensagem.msg, id: socket.id, nick: mensagem.username });
+    });
 });
+
+function updateUsers() {
+    const onlineUsers = users.map((user) => user.username);
+    io.emit("online", { count: users.length, users: onlineUsers });
+    console.log({ onlineUsers });
+}
 
 const PORT = process.env.PORT || 9000;
 
